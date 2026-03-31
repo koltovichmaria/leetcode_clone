c0a73dbf28fb66c903c08d69f3eda8d3b099b67f
koltovichmaria <maria4081123@gmail.com>
2026-03-31 23:36:12 +0300

    logout and reset password functionality

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { BsList } from "react-icons/bs";
 import { useRouter } from "next/router";
+import Logout from "../Buttons/Logout";
 
 
 type TopbarProps = {
@@ -15,8 +16,8 @@ type TopbarProps = {
 };
 
 const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
-	
-
+	const [user] = useAuthState(auth);
+	const setAuthModalState = useSetRecoilState(authModalState);
 	return (
 		<nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
 			<div className={`flex w-full items-center justify-between ${!problemPage ? "max-w-[1200px] mx-auto" : ""}`}>
@@ -36,11 +37,30 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 							Premium
 						</a>
 					</div>
-					<Link
+					{!user && (
+                    <Link
 							href='/auth'
+							onClick={() => {
+								setAuthModalState((prev) => ({...prev, isOpen: true, type: "login"}));
+							}}
 						>
 							<button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
 					</Link>
+                    )}
+                    {user && (
+                        <div className="cursor-pointer group relative">
+                            <img src="/avatar.png" alt="user profile img" className="h-8 w-8 rounded-full"/>
+
+                            <div
+								className='absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+								z-40 group-hover:scale-100 scale-0 
+								transition-all duration-300 ease-in-out'
+							>
+								<p className='text-sm'>{user.email}</p>
+							</div>
+                        </div>
+                    )}
+					{user && <Logout/>}
 				</div>
 			</div>
 		</nav>
