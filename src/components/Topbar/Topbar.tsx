c9878022e8d61a93e4e6b0a394c6cd6cddefe8fd
koltovichmaria <maria4081123@gmail.com>
2026-04-01 15:44:23 +0300

    creating [pid] page and updating topbar, creating Timer.tsx

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -6,6 +6,9 @@ import { useSetRecoilState } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
 import Image from "next/image";
 import Logout from "../Buttons/Logout";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { BsList } from "react-icons/bs";
+import Timer from "../Timer/Timer";
 
 
 type TopbarProps = {
@@ -22,7 +25,23 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 					<Image src='/logo-full.png' alt='Logo' height={100} width={100} />
 				</Link>
 
+				{problemPage && (
+					<div className="flex items-center gap-4 flex-1 justify-center"> 
+						<div className="flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer"> 
+							<FaChevronLeft /> 
+						</div>
 
+						<Link href="/" className="flex items-center gap-2 font-medium max-w-[170px] text-dark-gray-8 cursor-pointer">
+							<div>
+								<BsList />
+							</div>
+							<p>Problem List</p>
+						</Link>
+						<div className="flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer"> 
+							<FaChevronRight /> 
+						</div>
+					</div>
+				)}
 				<div className='flex items-center space-x-4 flex-1 justify-end'>
 					<div>
 						<a
@@ -44,6 +63,7 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 							<button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
 					</Link>
                     )}
+					{problemPage && <Timer/>}
                     {user && (
                         <div className="cursor-pointer group relative">
                             <img src="/avatar.png" alt="user profile img" className="h-8 w-8 rounded-full"/>
