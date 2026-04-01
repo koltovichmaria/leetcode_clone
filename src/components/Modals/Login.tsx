8f4b6a76f37a00c2518cb85b4bb9d6ff2ce79cd4
koltovichmaria <maria4081123@gmail.com>
2026-04-01 14:22:42 +0300

    added react-toastify to login and reset password components

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -4,6 +4,7 @@ import { useSetRecoilState } from 'recoil';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase/firebase';
 import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
 
 type LoginProps = {
     
@@ -40,7 +41,7 @@ const Login:React.FC<LoginProps> = () => {
     }
 
     useEffect(() => {
-        if (error) alert (error.message);
+        if (error) toast.error(error.message, {position: "top-center", autoClose: 3000, theme: 'dark'});
     }, [error])
 
     return <form className='space-y-6 px-6 pb-4' onSubmit={handleLogin}>
